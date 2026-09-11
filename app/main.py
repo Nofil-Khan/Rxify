647dc015ac647ad9c522a843fd2d7aa28c1b92ed
Nofil-Khan <nofilkhanro2008@gmail.com>
2026-09-11 21:32:07 +0530

    adding frontend url to middleware

--- app/main.py
+++ app/main.py
@@ -22,6 +22,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://localhost:3001",
+        "https://rxify-three.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
